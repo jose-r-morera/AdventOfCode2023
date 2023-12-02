16fe80cc56f29f7669c3e624eefd9c37665cbff7
José <alu0101471846@ull.edu.es>
2023-12-02 10:28:24 +0000

    Día 2 parte 2

--- 02/day2_2.py
+++ 02/day2_2.py
@@ -4,3 +4,30 @@
 # See:  https://adventofcode.com/2023/day/2
 # Date: 02/12/2023
 __author__ = "José Ramón Morera Campos"
+
+import re # regex
+
+file_name = input("Introduzca la ruta del archivo: ")
+
+try:
+  file = open(file_name, "r")
+except:
+  print("No se pudo abrir el fichero", file_name)
+  exit()
+
+cubes = ["red", "green", "blue"]
+cubes_power_sum = 0
+
+for line in file:
+  current_power = 1
+
+  for cube in cubes:
+    string_values = re.findall(r"(\d+) " + cube, line)
+    int_values = [int(value) for value in string_values]
+ 
+    current_power *= max(int_values)
+  
+  cubes_power_sum += current_power
+
+print("La suma de potencias de cubos es: " + str(cubes_power_sum))
+file.close()
