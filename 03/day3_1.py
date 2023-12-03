790521f557b52168a6239df12b56469ecdc0e72e
José <alu0101471846@ull.edu.es>
2023-12-03 16:27:21 +0000

    Comentarios y refactor

--- 03/day3_1.py
+++ 03/day3_1.py
@@ -8,48 +8,43 @@ __author__ = "José Ramón Morera Campos"
 import re # regex
 
 file_name = input("Introduzca la ruta del archivo: ")
-
 try:
   file = open(file_name, "r")
 except:
   print("No se pudo abrir el fichero", file_name)
   exit()
 
-# Leer todas las líneas del fichero
-lines = file.readlines()
+parts_sum = 0 # Resultado final
+lines = file.readlines() # Todas las líneas del fichero
 
-parts_sum = 0
+# Diccionario en el que para cada línea almacenamos pares [posición, número]
 line_numbers = {}
 for i in range(len(lines)):
   line_numbers[i] = []
 
 # Almacenamos los números de la primera línea
-for digit in re.finditer(r"\d+", lines[0]):
-      line_numbers[0].append(tuple([digit.start(), int(digit.group())]))
+for number in re.finditer(r"\d+", lines[0]):
+  line_numbers[0].append(tuple([number.start(), int(number.group())]))
 
 for line_index in range(len(lines)):
   # Almacenamos los números de la siguiente línea
   if(line_index + 1 < len(lines)):
-    for digit in re.finditer(r"\d+", lines[line_index+1]):
-      line_numbers[line_index+1].append(tuple([digit.start(), int(digit.group())]))
+    for number in re.finditer(r"\d+", lines[line_index+1]):
+      line_numbers[line_index+1].append(tuple([number.start(), int(number.group())]))
 
   for char_index in range(len(lines[line_index])-1):
     char = lines[line_index][char_index]
     if(char != "." and not char.isnumeric()):
       # Hay caracter especial, miramos adyacencia
       for i in [-1, 0, 1]:
-        # Obtenemos los números de la línea
-        numbers = line_numbers[line_index+i] 
+        numbers = line_numbers[line_index+i] # Números que hay en la línea
         for j in [-1, 0, 1]:
           if(i == 0 and j == 0):
             continue
-          for number in numbers:
+          for number in numbers:  # Para cada número de la línea vemos si es adyacente
             if(number[0] <= char_index+j and number[0]+(len(str(number[1]))-1) >= char_index+j):
               parts_sum += number[1] 
               numbers.remove(number)
       
-      print(char, char_index)
-      
-
 print("La suma de partes es: " + str(parts_sum))
 file.close()
--- 03/day3_2.py
+++ 03/day3_2.py
@@ -8,49 +8,47 @@ __author__ = "José Ramón Morera Campos"
 import re # regex
 
 file_name = input("Introduzca la ruta del archivo: ")
-
 try:
   file = open(file_name, "r")
 except:
   print("No se pudo abrir el fichero", file_name)
   exit()
 
-# Leer todas las líneas del fichero
-lines = file.readlines()
+gears_sum = 0 # Resultado final
+lines = file.readlines() # Todas las líneas del fichero
 
-gears_sum = 0
+# Diccionario en el que para cada línea almacenamos pares [posición, número]
 line_numbers = {}
 for i in range(len(lines)):
   line_numbers[i] = []
 
 # Almacenamos los números de la primera línea
-for digit in re.finditer(r"\d+", lines[0]):
-      line_numbers[0].append(tuple([digit.start(), int(digit.group())]))
+for number in re.finditer(r"\d+", lines[0]):
+  line_numbers[0].append(tuple([number.start(), int(number.group())]))
 
 for line_index in range(len(lines)):
   # Almacenamos los números de la siguiente línea
   if(line_index + 1 < len(lines)):
-    for digit in re.finditer(r"\d+", lines[line_index+1]):
-      line_numbers[line_index+1].append(tuple([digit.start(), int(digit.group())]))
+    for number in re.finditer(r"\d+", lines[line_index+1]):
+      line_numbers[line_index+1].append(tuple([number.start(), int(number.group())]))
 
-  for char_index in range(len(lines[line_index])-1):
+  for char_index in range(len(lines[line_index])-1): # Caracteres antes del salto de línea
     char = lines[line_index][char_index]
     if(char == "*"):
       # Hay un posible "engranaje", miramos adyacencia
       adjacent_count = 0
       current_gear_sum = 1
       for i in [-1, 0, 1]:
-        # Obtenemos los números de la línea
-        numbers = line_numbers[line_index+i] 
+        numbers = line_numbers[line_index+i] # Números que hay en la línea
         for j in [-1, 0, 1]:
-          if(i == 0 and j == 0):
+          if (i == 0 and j == 0):
             continue
-          for number in numbers:
-            if(number[0] <= char_index+j and number[0]+(len(str(number[1]))-1) >= char_index+j):
+          for number in numbers:  # Para cada número de la línea vemos si es adyacente
+            if (number[0] <= char_index+j and number[0]+(len(str(number[1]))-1) >= char_index+j):
               numbers.remove(number)
               adjacent_count += 1
               current_gear_sum *= number[1]
-      if(adjacent_count == 2):
+      if (adjacent_count == 2):
         gears_sum += current_gear_sum
 
 print("La suma de partes es: " + str(gears_sum))
