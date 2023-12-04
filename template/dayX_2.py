17fb3276ea4f32cf6ee9ac5faef98fae56fc3888
José <alu0101471846@ull.edu.es>
2023-12-04 19:28:27 +0000

    Plantilla

---
+++ template/dayX_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Dia X parte 2
+# See:  https://adventofcode.com/2023/day/X
+# Date: XX/12/2023
+__author__ = "José Ramón Morera Campos"
+
+import re # regex
+
+file_name = input("Introduzca la ruta del archivo: ")
+
+try:
+  file = open(file_name, "r")
+except:
+  print("No se pudo abrir el fichero", file_name)
+  exit()
+
+# Total
+points_sum = 0
+
+print("La suma de puntos es: " + str(points_sum))
+file.close()
