80263fb6f0ad39ce600c318813b8f930bb8c682d
José <alu0101471846@ull.edu.es>
2023-12-07 22:40:59 +0000

    Legibilidad

--- 07/day7_2.py
+++ 07/day7_2.py
@@ -17,8 +17,8 @@ except:
   exit()
 
 cards = {}  # Representamos cada carta como una dupla [bid, valor_numérico]
-for card in file:
-  value, bid = re.findall(r"(.*) (.*)", card)[0]
+for line in file:
+  value, bid = re.findall(r"(.*) (.*)", line)[0]
   cards[value] = [bid, 0]
 
 face_points = {"A":13, "K":12, "Q":11, "J":0, "T":9, '9':8, '8':7, '7':6, '6':5, '5':4, '4':3, '3':2, '2':1}
