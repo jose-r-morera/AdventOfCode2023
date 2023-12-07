3a14b7cf6772e3bed15724a360fd9f697963fd5b
José <alu0101471846@ull.edu.es>
2023-12-07 22:34:09 +0000

    Refactor

--- 07/day7_2.py
+++ 07/day7_2.py
@@ -16,51 +16,35 @@ except:
   print("No se pudo abrir el fichero", file_name)
   exit()
 
-cards = {}
+cards = {}  # Representamos cada carta como una dupla [bid, valor_numérico]
 for card in file:
   value, bid = re.findall(r"(.*) (.*)", card)[0]
   cards[value] = [bid, 0]
 
-for card_value in cards:
-  joker = 0
-  value_count = {"A":0, "K":0, "Q":0, "T":0, '9':0, '8':0, '7':0, '6':0, '5':0, '4':0, '3':0, '2':0}
-  for digit in card_value:
-    if digit == "J":
-      joker += 1
-    else:
-      value_count[digit] += 1
-  most_repeated_card_cound = max(value_count.values()) + joker
-  value_count.pop(max(value_count, key=value_count.get))
-  second_repeated_card_count = max(value_count.values())
-
-  # Se podria hacer con un array y un enumerate
-  if(most_repeated_card_cound == 5): 
-    cards[card_value][1] = 60000000000
-  elif (most_repeated_card_cound == 4):
-    cards[card_value][1] = 50000000000
-  elif (most_repeated_card_cound == 3 and second_repeated_card_count == 2): # Full house
-    cards[card_value][1] = 40000000000
-  elif (most_repeated_card_cound == 3 and second_repeated_card_count == 1): 
-    cards[card_value][1] = 30000000000
-  elif (most_repeated_card_cound == 2 and second_repeated_card_count == 2): 
-    cards[card_value][1] = 20000000000
-  elif (most_repeated_card_cound == 2): 
-    cards[card_value][1] = 10000000000
-  else:
-    cards[card_value][1] = 0
-
 face_points = {"A":13, "K":12, "Q":11, "J":0, "T":9, '9':8, '8':7, '7':6, '6':5, '5':4, '4':3, '3':2, '2':1}
 for card_value in cards:
+  joker = 0 # Contamos los joker como comodín
+  value_count = {"A":0, "K":0, "Q":0, "T":0, '9':0, '8':0, '7':0, '6':0, '5':0, '4':0, '3':0, '2':0}
+  for digit in card_value:
+    if digit == "J": joker += 1
+    else:   value_count[digit] += 1 # Establecemos cuántas cartas de cada tipo tiene la mano
+  most_repeated_card_cound = max(value_count.values()) + joker  # Cantidad máxima de repeticiones, incluimos los joker
+  value_count.pop(max(value_count, key=value_count.get))
+  second_repeated_card_count = max(value_count.values()) # Segundo máximo de repeticiones
+
+  for i, values in enumerate([[5, 0], [4, 1], [3, 2], [3, 1], [2, 2], [2, 1], [1, 1]]): # Combinaciones de las manos, en orden decreciente
+    if([most_repeated_card_cound, second_repeated_card_count] == values): 
+      cards[card_value][1] = (6-i)*10000000000  # Asignamos valor según el tipo de mano
+      break
+  
   pow = 1
-  for digit in range(0,5):
+  for digit in range(0,5):  # Asignamos valor según cada dígito
     cards[card_value][1] += face_points[card_value[4-digit]] * pow
     pow *= 100
 
-rank = 1
 total_winnings = 0
-for card in sorted(cards.items(), key=lambda item: item[1][1]):
-  total_winnings += int(card[1][0]) * rank
-  rank += 1
+for index, card in enumerate(sorted(cards.items(), key=lambda item: item[1][1])):
+  total_winnings += int(card[1][0]) * (index + 1)
 
 print("La suma de puntos es: " + str(total_winnings))
 file.close()
